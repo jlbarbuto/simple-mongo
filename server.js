c39b7716088b90f041b22f31c1b56419c9c49bd1
unknown <jlbarbut@ncsu.edu>
2018-07-01 18:32:40 -0400

    scrape and articles routes added

--- server.js
+++ server.js
@@ -22,6 +22,42 @@ mongoose.connect("mongodb://localhost/simpleMongo");
 
 // Routes =====================================
 
+//route scrapes the web for articles
+app.get("/scrape", function(req, res){
+    request("http://www.technicianonline.com/news/", function(error, response, html){
+        var $ = cheerio.load(html);
+
+        $("h3.tnt-headline").each(function(i, element){
+            var result = {};
+
+            result.title = $(element).children("a").text();
+            result.link = $(element).children("a").attr("href");
+
+            db.Article.create(result)
+                .then(function(dbArticle){
+                    console.log(dbArticle);
+                })
+                .catch(function(err){
+                    return res.json(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+//route gets all the articles from the db
+app.get("/articles", function(req, res){
+    //use the .find method to grab all articles from the db
+    db.Article.find({})
+        .then(function(dbArticle){
+            //send success back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err){
+            //send error back to the client
+            res.json(err);
+        });
+});
 
 // Start server
 app.listen(PORT, function() {
